f5b76b0bedd7ce815d078e4b14bafe079295f56c
veltodorov <willking66@gmail.com>
2022-03-25 20:19:03 +0100

    :hammer: refactor `ItemFactory`
    add single returning parameter

--- src/main/java/com/gildedrose/item_helpers/ItemFactory.java
+++ src/main/java/com/gildedrose/item_helpers/ItemFactory.java
@@ -1,5 +1,8 @@
 package com.gildedrose.item_helpers;
 
+import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Stream.of;
+
 import com.gildedrose.items.AgedBrieItem;
 import com.gildedrose.items.BackstagePassItem;
 import com.gildedrose.items.ConjuredItem;
@@ -7,28 +10,24 @@ import com.gildedrose.items.LegendaryItem;
 import com.gildedrose.items.NormalItem;
 import com.gildedrose.main.Item;
 
-import java.util.Map;
-
-import static java.util.stream.Collectors.toMap;
-import static java.util.stream.Stream.of;
-
 public final class ItemFactory {
 
   private ItemFactory() {
   }
 
   public static ItemType getItemType(Item item) {
-    ItemType itemType = getItems(item).get(item.name);
+    ItemType itemType = getItem(item);
     if (itemType == null) {
       itemType = new NormalItem(item);
     }
     return itemType;
   }
 
-  private static Map<String, ItemType> getItems(Item item) {
+  private static ItemType getItem(Item item) {
     return of(new NormalItem(item), new AgedBrieItem(item), new LegendaryItem(item),
         new BackstagePassItem(item), new ConjuredItem(item))
-        .collect(toMap(ItemType::getName, itemType -> itemType));
+        .collect(toMap(ItemType::getName, itemType -> itemType))
+        .get(item.name);
   }
 
 }
