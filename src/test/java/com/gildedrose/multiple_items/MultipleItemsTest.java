4df2d1b3ea5bf44df5527e75d6e2bb32ee1c0c66
Velizar Todorov <willking66@gmail.com>
2022-10-19 02:21:46 +0200

    :hammer: :white_check_mark: refactor `MultipleItemsTest`

--- src/test/java/com/gildedrose/multiple_items/MultipleItemsTest.java
+++ src/test/java/com/gildedrose/multiple_items/MultipleItemsTest.java
@@ -4,33 +4,39 @@ import static com.gildedrose.items.AgedBrieItemTest.AGED_BRIE;
 import static com.gildedrose.items.BackstagePassItemTest.BACKSTAGE_PASS;
 import static com.gildedrose.items.ConjuredItemTest.CONJURED;
 import static com.gildedrose.items.LegendaryItemTest.LEGENDARY;
+import static lombok.AccessLevel.PRIVATE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 
 import com.gildedrose.main.GildedRose;
 import com.gildedrose.main.Item;
+import lombok.experimental.FieldDefaults;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestMethodOrder;
 
 @TestMethodOrder(OrderAnnotation.class)
+@FieldDefaults(makeFinal = true, level = PRIVATE)
+@TestInstance(PER_CLASS)
 class MultipleItemsTest {
 
-  public static final String DEXTERITY_VEST = "+5 Dexterity Vest";
-  public static final String ELIXIR_OF_THE_MONGOOSE = "Elixir of the Mongoose";
+  String DEXTERITY_VEST = "+5 Dexterity Vest";
+  String ELIXIR_OF_THE_MONGOOSE = "Elixir of the Mongoose";
 
-  public static final Item normalItem1 = new Item(DEXTERITY_VEST, 10, 20);
-  public static final Item agedBrieItem = new Item(AGED_BRIE, 2, 0);
-  public static final Item normalItem2 = new Item(ELIXIR_OF_THE_MONGOOSE, 5, 7);
-  public static final Item legendaryItem1 = new Item(LEGENDARY, 0, 80);
-  public static final Item legendaryItem2 = new Item(LEGENDARY, -1, 80);
-  public static final Item backStagePassItem1 = new Item(BACKSTAGE_PASS, 15, 20);
-  public static final Item backStagePassItem2 = new Item(BACKSTAGE_PASS, 10, 49);
-  public static final Item backStagePassItem3 = new Item(BACKSTAGE_PASS, 5, 49);
-  public static final Item conjuredItem = new Item(CONJURED, 3, 6);
+  Item normalItem1 = new Item(DEXTERITY_VEST, 10, 20);
+  Item agedBrieItem = new Item(AGED_BRIE, 2, 0);
+  Item normalItem2 = new Item(ELIXIR_OF_THE_MONGOOSE, 5, 7);
+  Item legendaryItem1 = new Item(LEGENDARY, 0, 80);
+  Item legendaryItem2 = new Item(LEGENDARY, -1, 80);
+  Item backStagePassItem1 = new Item(BACKSTAGE_PASS, 15, 20);
+  Item backStagePassItem2 = new Item(BACKSTAGE_PASS, 10, 49);
+  Item backStagePassItem3 = new Item(BACKSTAGE_PASS, 5, 49);
+  Item conjuredItem = new Item(CONJURED, 3, 6);
 
-  public static Item[] getMultipleItems() {
+  private Item[] getMultipleItems() {
     return new Item[]{normalItem1, agedBrieItem, normalItem2,
         legendaryItem1, legendaryItem2,
         backStagePassItem1, backStagePassItem2, backStagePassItem3,
@@ -38,7 +44,7 @@ class MultipleItemsTest {
   }
 
   @BeforeAll
-  static void updateItemsQualityFor20Days() {
+  void updateItemsQualityFor20Days() {
     Item[] items = getMultipleItems();
     GildedRose gildedRose = new GildedRose(items);
     int days = 20;
